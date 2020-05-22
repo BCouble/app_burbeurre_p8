30cbfb44f25c74b97006908402748c7a8c110eab
bab-graph <baptiste.couble@free.fr>
2020-05-22 13:10:11 +0200

    Test view search substitute

--- purbeurre/tests/test_views/test_search_substitute.py
+++ purbeurre/tests/test_views/test_search_substitute.py
@@ -18,7 +18,7 @@ class SearchFoodPage(TestCase):
         FoodPurBeurre.objects.create(
             product_name_fr='mini nutella',
             generic_name_fr='plus que mini nutella',
-            nutriscore='e',
+            nutriscore='c',
             nut_cent_gr=123,
             category_s1=cat_nutella,
             store='usa',
@@ -45,27 +45,50 @@ class SearchFoodPage(TestCase):
             link_off='http://vivelebeurre.fr',
             link_img='http://vivelebeurre.fr/bignut_img.jpeg',
         )
+        FoodPurBeurre.objects.create(
+            product_name_fr='choco mou',
+            generic_name_fr='plus que mini nutella',
+            nutriscore='b',
+            nut_cent_gr=113,
+            category_s1=cat_nutella,
+            store='usa',
+            link_off='http://nute.fr',
+            link_img='http://nute.fr/mininut_img.jpeg',
+        )
+        FoodPurBeurre.objects.create(
+            product_name_fr='moelle choc',
+            generic_name_fr='',
+            nutriscore='a',
+            nut_cent_gr=90,
+            category_s1=cat_nutella,
+            store='usa',
+            link_off='http://nute.fr',
+            link_img='http://nute.fr/bignut_img.jpeg',
+        )
+        query = FoodPurBeurre.objects.get(product_name_fr='big nutella')
+        self.pk = query.id
+        query_pizza = FoodPurBeurre.objects.get(product_name_fr='pizza chorizo')
+        self.pk2 = query_pizza.id
 
     def test_index_returns_200(self):
         """ Test statut code IndexView"""
-        response = self.client.get(reverse('purbeurre:search_food'), {'search_text': 'nutella'})
+        response = self.client.get(reverse('purbeurre:search_substitute', args=(self.pk,)))
         self.assertEqual(response.status_code, 200)
 
     def test_view_with_resolver_match(self):
         """ Test view search food with request : nutella """
-        data = {'search_text': 'nutella'}
-        response = self.client.get('/purbeurre/search_food/', data)
+        response = self.client.get(reverse('purbeurre:search_substitute', args=(self.pk,)))
         self.assertEqual(response.resolver_match.func.__name__, SearchSubstituteView.as_view().__name__)
 
-    def test_search_text_empty(self):
-        """ Queryset test with empty keyword """
-        response = self.client.get(reverse('purbeurre:search_food'), {'search_text': ''})
-        self.assertContains(response, "Nous n\'avons pas trouvé de produit correspondant à votre recherche !")
-        self.assertQuerysetEqual(response.context['search_foods'], [])
+    def test_search_zero_result(self):
+        """ Queryset test with one response """
+        response = self.client.get(reverse('purbeurre:search_substitute', args=(self.pk2,)))
+        self.assertContains(response, "Nous n'avons pas trouvé de substitus !")
+        self.assertQuerysetEqual(response.context['search_substitute'], ['<FoodPurBeurre: pizza chorizo>'])
 
     def test_search_text_one_key(self):
         """ Queryset test with 2 products in result """
-        response = self.client.get(reverse('purbeurre:search_food'), {'search_text': 'nutella'}, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        response = self.client.get(reverse('purbeurre:search_substitute', args=(self.pk,)), HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         self.assertQuerysetEqual(list(response), (repr(r) for r in response))
 
 
